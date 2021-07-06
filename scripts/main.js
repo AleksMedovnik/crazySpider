4a4e2032cc15b6b150a09494cb944cdfdafc6156
Aleksandr <medovnikaleksander@yandex.ua>
2021-07-06 21:53:28 +0300

    Added first commit

---
+++ scripts/main.js
@@ -0,0 +1,61 @@
+const container = document.getElementById('container');
+const app = new PIXI.Application({
+    width: 540,
+    height: 320,
+});
+container.appendChild(app.view);
+
+app.stop();
+
+app.loader
+    .add(['images/grass.jpg',
+        'images/spiders/spiders.json'])
+    .load(onAssetsLoaded);
+
+function onAssetsLoaded() {
+    const spiderTextures = [];
+    let timer = 30;
+
+    let grass = new PIXI.Sprite(
+        app.loader.resources['images/grass.jpg'].texture
+    );
+
+    grass.width = app.screen.width;
+    grass.height = app.screen.height;
+
+    app.stage.addChild(grass);
+
+
+
+    for (let i = 0; i < 256; i++) {
+        const texture = PIXI.Texture.from(
+            `spider_${i}.png`
+        );
+        spiderTextures.push(texture);
+    }
+
+    const spider = new PIXI.AnimatedSprite(spiderTextures);
+
+    spider.x = 50;
+    spider.y = 50;
+    spider.width = 128;
+    spider.height = 128;
+    spider.anchor.set(0.5);
+    spider.gotoAndPlay(0);
+    spider.animationSpeed = .3;
+
+    app.stage.addChild(spider);
+
+    app.ticker.add(() => {
+        if (timer > 0) {
+            timer--;
+        } else {
+            spider.x = Math.random() * (app.screen.width - 50);
+            spider.y = Math.random() * (app.screen.height - 50);
+            timer = 30;
+        }
+    });
+
+
+    app.start();
+}
